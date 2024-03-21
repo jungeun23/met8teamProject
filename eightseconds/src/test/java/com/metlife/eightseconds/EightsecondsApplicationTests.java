e95f73fba93f546c310c255783284c5e755667b4
jungeun23 <mnms11@naver.com>
2024-03-21 23:07:53 +0900

    초기 패키지

---
+++ eightseconds/src/test/java/com/metlife/eightseconds/EightsecondsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metlife.eightseconds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EightsecondsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
